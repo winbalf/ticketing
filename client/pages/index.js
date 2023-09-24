12f5ca6f3427ee484c8f38ec9cb3163286110312
Edwin <edwin@gmail.com>
2023-09-24 21:09:26 +0930

    new startup message

--- client/pages/index.js
+++ client/pages/index.js
@@ -17,7 +17,7 @@ const LandingPage = ({ currentUser, tickets }) => {
 
   return (
     <div>
-      <h1>TIckets</h1>
+      <h2>TIckets</h2>
       <table className="table">
         <thead>
           <tr>
